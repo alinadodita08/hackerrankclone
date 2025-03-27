2ad6aee1d9f6afff8122bde0c5b8cff47af00360
Alina Dodita <alinadodita08@gmail.com>
2025-03-27 17:51:31 +0000

    add monaco editor

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
+import Editor from '@monaco-editor/react';
 
 function App() {
   return (
-    <h1>
-      hello world
-    </h1>
-    
+    <> 
+    <Editor height="50vh" defaultLanguage="python" defaultValue="// some comment" />
+    <button>Submit</button>
+    </>
+   
   )
 }
 
