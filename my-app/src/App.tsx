3e8fa327df768a576dbd56928a7ded0a1cc21922
Alina Dodita <alinadodita08@gmail.com>
2025-04-25 14:33:24 +0100

    added xtrm

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Editor from '@monaco-editor/react';
+
 import './App.css'
 
 const languages = [
---
+++ my-app/src/components/terminal.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { XTerm } from 'react-xtermjs';
+
+const MyTerminal = () => {
+    const onData = (data) => {
+        console.log(`Received data: ${data}`);
+    };
+
+    const onResize = (cols, rows) => {
+        console.log(`Terminal resized to ${cols} columns and ${rows} rows`);
+    };
+
+    return (
+        <XTerm
+            onData={onData}
+            onResize={onResize}
+            options={{ cursorBlink: true }}
+            style={{ width: '100%', height: '100%' }}
+        />
+    );
+};
+
+export default MyTerminal;
